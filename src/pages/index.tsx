2d97c41df7cf608fad57d5d83e908f3079eb0cc2
Sumpah Gw Bukan Lolicon <51400321+xyelea@users.noreply.github.com>
2023-04-22 00:13:03 +0700

    optimize index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,20 @@
-import { supabase } from "../utils/supabase";
+import { supabase } from "@/utils/supabase";
+import { LessonType } from "@/types/collection";
 import Link from "next/link";
 
-interface Lesson {
-  id: number;
-  title: string;
-  description: string;
-  created_at: string;
-}
-
-interface Props {
-  lessons: Lesson[];
-}
-
-export default function Home({ lessons }: Props) {
-  async function getUserData() {
-    try {
-      const data = await supabase.auth.getUser();
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  getUserData();
-
+export default function Home({ lessons }: { lessons: LessonType[] }) {
+  // console.log(lessons);
   return (
-    <main className="w-full max-w-3xl mx-auto my-16 px-2">
+    <div className="w-full max-w-3xl mx-auto my-16 px-2">
       {lessons.map((lesson) => (
-        <Link key={lesson.id} href={`/${lesson.id}`}>
-          <p className="p-8 h-40 mb-4 rounded shadow text-xl flex">
-            {lesson.title}
-          </p>
+        <Link
+          href={`/${lesson.id}`}
+          key={lesson.id}
+          className="p-8 h-40 mb-4 rounded shadow text-xl flex">
+          {lesson.title}
         </Link>
       ))}
-    </main>
+    </div>
   );
 }
 

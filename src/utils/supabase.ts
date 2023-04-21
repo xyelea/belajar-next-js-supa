b876fde360ef4783eff18cb27361da77629ec9d4
Sumpah Gw Bukan Lolicon <51400321+xyelea@users.noreply.github.com>
2023-04-22 00:13:26 +0700

    optimize pathing of types

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import { Database } from "../types/supabase";
+import { Database } from "@/types/supabase";
 export const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL as string,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string

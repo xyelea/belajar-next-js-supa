3391155bdb07d4d1f4e1897c49616a8078f95e9e
Sumpah Gw Bukan Lolicon <51400321+xyelea@users.noreply.github.com>
2023-05-01 09:23:30 +0700

    fix extrasemicolon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import UserProvider from "@/context/user";
+import Nav from "@/components/nav";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
-      <Component {...pageProps} />;
+      <Nav />
+      <Component {...pageProps} />
     </UserProvider>
   );
 }

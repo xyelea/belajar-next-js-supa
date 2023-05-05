c638ffe9e09e876a18f6399064ac7dd5a74573de
Sumpah Gw Bukan Lolicon <51400321+xyelea@users.noreply.github.com>
2023-05-05 15:42:20 +0700

    feat: replace auth helpers in App component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,28 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
+import { useState } from "react";
 import UserProvider from "@/context/user";
 import Nav from "@/components/nav";
+import { Database } from "@/types/supabase";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{ initialSession: Session }>) {
+  const [supabase] = useState(() => createBrowserSupabaseClient<Database>());
+
   return (
-    <UserProvider>
-      <Nav />
-      <Component {...pageProps} />
-    </UserProvider>
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}>
+      <UserProvider>
+        <main>
+          <Nav />
+          <Component {...pageProps} />
+        </main>
+      </UserProvider>
+    </SessionContextProvider>
   );
 }

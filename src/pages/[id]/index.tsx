f4b048596c37e5dc537a73190ce8867d247c3060
Sumpah Gw Bukan Lolicon <51400321+xyelea@users.noreply.github.com>
2023-05-05 22:09:13 +0700

    refactor: Refaktor kode pada halaman LessonDetails

--- src/pages/[id]/index.tsx
+++ src/pages/[id]/index.tsx
@@ -1,75 +1,88 @@
-import { supabase } from "@/utils/supabase";
+import { GetServerSidePropsContext } from "next";
+import { useState, useEffect } from "react";
 import { LessonType } from "@/types/collection";
-import { PremiumContentType } from "@/types/collection";
-import { useEffect, useState } from "react";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import Video from "react-player";
+import { Database } from "@/types/supabase";
+import { useUserContext } from "@/context/user";
+// import { Lesson } from "./index";
 
-const LessonDetails = ({ lesson }: { lesson: LessonType }) => {
-  const [videoUrl, setVideoUrl] = useState<string>();
+export default function LessonDetails({ lesson }: { lesson: LessonType }) {
+  const [videoUrl, setVideoUrl] = useState<string | null>(null);
+  const supabaseClient = useSupabaseClient<Database>();
+  const { user } = useUserContext();
+  console.log("lesson", lesson);
 
   const getPremiumContent = async () => {
-    const { data } = await supabase
+    const { data } = await supabaseClient
       .from("premium_content")
       .select("video_url")
       .eq("id", lesson.id)
       .single();
 
-    if (data?.video_url) {
-      setVideoUrl(data.video_url);
-    }
+    console.log("GET VIDEO DATA", data);
+
+    setVideoUrl(data?.video_url as string);
   };
 
-  console.log(lesson);
-
   useEffect(() => {
-    getPremiumContent();
+    if (user && user.is_subscribed) {
+      getPremiumContent();
+    }
   }, []);
 
   return (
-    <div className="w-full max-w-3xl mx-auto py-16 px8">
-      <h1 className="text-3xl mb-6">{lesson.title}</h1>
-      <p className="mb-8">{lesson.description}</p>
-      {!!videoUrl && <Video url={videoUrl} width="100%" controls />}
-      {!videoUrl && (
-        <p className="py-6 px-4 rounded bg-purple-100 border border-solid border-violet-400 text-violet-600 text-center">
-          Silahkan berlangganan terlebih dahulu sebelum melihat konten
-        </p>
+    <>
+      <div className="w-full max-w-3xl mx-auto py-16 px-8">
+        <h1 className="text-3xl mb-6">{lesson?.title}</h1>
+        <p className="mb-8">{lesson?.description}</p>
+        {!!videoUrl && <Video url={videoUrl} width="100%" />}
+        {!videoUrl && (
+          <p className="py-6 px-4 rounded bg-purple-100 border border-solid border-violet-400 text-violet-600 text-center">
+            Subscribe to view premium content
+          </p>
+        )}
+      </div>
+      {user && (
+        <pre className="w-full max-w-3xl mx-auto my-8 px-2 text-center text-red-700">
+          {JSON.stringify(user, null, 2)}
+        </pre>
       )}
-    </div>
+    </>
   );
-};
+}
 
-type Params = {
-  params: {
-    id: string;
-  };
-};
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const { id } = ctx.params as { id: string };
 
-export const getStaticPaths = async () => {
-  const { data: lessons } = await supabase.from("lesson").select("id");
+  const supabaseServerClient = createServerSupabaseClient<Database>(ctx);
 
-  const paths = lessons?.map(({ id }) => ({
-    params: { id: id.toString() },
-  }));
+  const {
+    data: { session },
+  } = await supabaseServerClient.auth.getSession();
 
-  return {
-    paths,
-    fallback: false,
-  };
-};
-
-export const getStaticProps = async ({ params: { id } }: Params) => {
-  const { data: lesson } = await supabase
+  const { data: lesson } = await supabaseServerClient
     .from("lesson")
-    .select()
+    .select("*")
     .eq("id", id)
     .single();
+
+  console.log("id - lesson", lesson);
+
+  if (!session) {
+    return {
+      props: {
+        lesson,
+      },
+    };
+  }
 
   return {
     props: {
       lesson,
+      initialSession: session,
+      user: session.user,
     },
   };
 };
-
-export default LessonDetails;
